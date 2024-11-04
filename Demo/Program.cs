0427c45c831e250c8b588305870c77a9b5dbc704
Neo <neo@mail.kway.com.tw>
2024-11-04 14:32:04 +0800

    <新增>打印改為當下時間為⺠國

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-Console.WriteLine(DateTime.Now.ToString("yyyyMMddHH:mm:ss.fff"));
+var now = DateTime.Now;
+var rocYear = now.Year-1911;
+var nowTime = now.ToString("MMddHH:mm:ss.fff");
+
+Console.WriteLine(rocYear+nowTime);
